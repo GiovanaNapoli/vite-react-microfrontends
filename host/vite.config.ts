11a675caf25ebda9837f8d9d7231885e9a49a47f
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-07-30 15:11:35 -0300

    feat: define vite.config with module federation

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "app",
+      remotes: {
+        authModule: "http://localhost:2020/assets/remoteEntry.js",
+        dashboardModule: "http://localhost:4000/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
